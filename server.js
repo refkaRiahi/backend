615366e240aa08597ecf51b213751ca37441a6a3
refkaRiahi <148687625+refkaRiahi@users.noreply.github.com>
2026-03-23 14:31:15 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
 const express = require("express");
-const nodemailer = require("nodemailer");
 const cors = require("cors");
 require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configurer SendGrid
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.post("/send-email", async (req, res) => {
   const { fullName, email, phone, message } = req.body;
 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USERNAME, // TON email
-      pass:  process.env.SMTP_PASSWORD  // Mot de passe d’application Gmail
-    }
-  });
-
-try {
-  await transporter.sendMail({
-    from: `"${fullName} <${email}>" <refka.riahy@gmail.com>`,
-    replyTo: email,
-    to: "riahiriahi762@gmail.com",
+  const msg = {
+    to: "riahiriahi762@gmail.com", // ton email de réception
+    from: "refka.riahy@gmail.com", // email validé sur SendGrid
     subject: "Nouveau message client",
-    html: `<p>Nom: ${fullName}</p><p>Email: ${email}</p><p>Téléphone: ${phone}</p><p>Message: ${message}</p>`
-  });
-  
-  console.log("✅ Mail envoyé avec succès !");
-  res.json({ ok: true, sent: "true" });
+    html: `
+      <div style="background:#742F17; padding:20px; font-family:Arial; color:black;">
+        <h2 style="color:white;">Nouveau message client</h2>
+        <p><strong>Nom :</strong> ${fullName}</p>
+        <p><strong>Téléphone :</strong> ${phone}</p>
+        <p><strong>Email :</strong> ${email}</p>
+        <p><strong>Message :</strong></p>
+        <p>${message}</p>
+      </div>
+    `
+  };
 
-} catch (error) {
-  console.error("❌ Nodemailer erreur:", error);
-  res.status(500).json({ error: "Erreur d'envoi", details: error.message });
-}
+  try {
+    await sgMail.send(msg);
+    console.log("✅ Mail envoyé avec succès !");
+    res.json({ ok: true, sent: "true" });
+  } catch (error) {
+    console.error("❌ SendGrid erreur:", error);
+    res.status(500).json({ error: "Erreur d'envoi", details: error.message });
+  }
 });
 
+// Port dynamique pour Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur démarré sur ${PORT}`));
